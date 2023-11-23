4daa8bf645bece857fbf1f5d35b8061e9494610f
Олег Цыпуштанов <madeit@MacBook-Pro-Oleg.local>
2023-11-23 07:09:21 +0300

    feat: connect body-parser to the app.ts file

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import usersRouter from './routes/users';
 const { PORT = 3000 } = process.env;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -14,12 +14,9 @@ router.get('/:userId', (req, res) => User.findById(req.params.userId)
 router.post('/', (req, res) => User.create({
   name: req.body.name,
   avatar: req.body.avatar,
+  about: req.body.about,
 })
   .then((user) => res.send(user))
   .catch((err) => res.status(400).send(err)));
 
 export default router;
-
-// GET /users — возвращает всех пользователей
-// GET /users/:userId - возвращает пользователя по _id
-// POST /users — создаёт пользователя
