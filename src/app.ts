09db18fa9706870aeb891929d870afcf0b47be21
Олег Цыпуштанов <madeit@MacBook-Pro-Oleg.local>
2023-11-25 16:38:21 +0300

    feat: create routes for card  model

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-import express from 'express';
+import express, { Response, NextFunction } from 'express';
 import mongoose from 'mongoose';
 
+import { CustomRequest } from './types/customRequestType';
 import usersRouter from './routes/users';
+import cardsRouter from './routes/cards';
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
@@ -12,7 +14,16 @@ app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+// временное решение
+app.use((req: CustomRequest, _res: Response, next: NextFunction) => {
+  req.user = {
+    _id: '655e6396121311960153d675',
+  };
+  next();
+});
+
 app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
---
+++ src/routes/cards.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import Card from '../models/card';
+import { CustomRequest } from '../types/customRequestType';
+
+const router = Router();
+
+router.get('/', (_req, res) => Card.find({})
+  .then((card) => res.send({ data: card }))
+  .catch(() => res.status(400).send({ message: 'Произошла ошибка' })));
+
+router.post('/', (req: CustomRequest, res) => {
+  const id = req.user?._id;
+  return Card.create({
+    name: req.body.name,
+    link: req.body.link,
+    owner: id,
+    likes: [],
+  })
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(400).send(err));
+});
+
+router.delete('/:cardId', (req, res) => Card.findByIdAndDelete(req.params.cardId)
+  .then(() => res.send({ message: 'Карточка удалена успешно' }))
+  .catch(() => res.status(400).send({ message: 'Произошла ошибка' })));
+
+export default router;
