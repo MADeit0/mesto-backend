48222f17309218ed047c214de581920350889060
Олег Цыпуштанов <madeit@MacBook-Pro-Oleg.local>
2023-11-25 20:47:18 +0300

    feat: create routes to update user data

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import User from '../models/user';
+import { CustomRequest } from '../types/customRequestType';
 
 const router = Router();
 
@@ -16,7 +17,23 @@ router.post('/', (req, res) => User.create({
   avatar: req.body.avatar,
   about: req.body.about,
 })
-  .then((user) => res.send(user))
+  .then((user) => res.send({ data: user }))
   .catch((err) => res.status(400).send(err)));
+
+router.patch('/me', (req: CustomRequest, res) => {
+  const userData = {
+    name: req.body.name,
+    about: req.body.about,
+  };
+  const notEmptyData = Object.fromEntries(Object.entries(userData).filter(([, value]) => value));
+
+  return User.findByIdAndUpdate(req.user?._id, notEmptyData)
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(400).send({ message: 'Пользователь не существует' }));
+});
+
+router.patch('/me/avatar', (req: CustomRequest, res) => User.findByIdAndUpdate(req.user?._id, { avatar: req.body.avatar }, { new: true })
+  .then((user) => res.send({ data: user }))
+  .catch(() => res.status(400).send({ message: 'Пользователь не существует' })));
 
 export default router;
