adfc1ea8cecf8112736a47ba1defcab1eee79010
Олег Цыпуштанов <madeit@MacBook-Pro-Oleg.local>
2023-11-26 10:22:34 +0300

    feat: divide and move the functionality of routes into controllers

--- src/routes/cards.ts
+++ src/routes/cards.ts
@@ -1,30 +1,17 @@
 import { Router } from 'express';
-import Card from '../models/card';
-import { CustomRequest } from '../types/customRequestType';
-import { likeCard, dislikeCard } from '../controllers/card';
+import {
+  likeCard,
+  dislikeCard,
+  findCards,
+  createCard,
+  deleteCard,
+} from '../controllers/cards';
 
 const router = Router();
 
-router.get('/', (_req, res) => Card.find({})
-  .then((card) => res.send({ data: card }))
-  .catch(() => res.status(400).send({ message: 'Произошла ошибка' })));
-
-router.post('/', (req: CustomRequest, res) => {
-  const id = req.user?._id;
-  return Card.create({
-    name: req.body.name,
-    link: req.body.link,
-    owner: id,
-    likes: [],
-  })
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(400).send(err));
-});
-
-router.delete('/:cardId', (req, res) => Card.findByIdAndDelete(req.params.cardId)
-  .then(() => res.send({ message: 'Карточка удалена успешно' }))
-  .catch(() => res.status(400).send({ message: 'Произошла ошибка' })));
-
+router.post('/', createCard);
+router.get('/', findCards);
+router.delete('/:cardId', deleteCard);
 router.put('/:cardId/likes', likeCard);
 router.delete('/:cardId/likes', dislikeCard);
 
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,39 +1,14 @@
 import { Router } from 'express';
-import User from '../models/user';
-import { CustomRequest } from '../types/customRequestType';
+import {
+  createUser, findUserById, findUsers, updateUserAvatar, updateUserData,
+} from '../controllers/users';
 
 const router = Router();
 
-router.get('/', (_req, res) => User.find({})
-  .then((user) => res.send({ data: user }))
-  .catch(() => res.status(500).send({ message: 'Произошла ошибка' })));
-
-router.get('/:userId', (req, res) => User.findById(req.params.userId)
-  .then((user) => res.send({ data: user }))
-  .catch(() => res.status(500).send({ message: 'Произошла ошибка' })));
-
-router.post('/', (req, res) => User.create({
-  name: req.body.name,
-  avatar: req.body.avatar,
-  about: req.body.about,
-})
-  .then((user) => res.send({ data: user }))
-  .catch((err) => res.status(400).send(err)));
-
-router.patch('/me', (req: CustomRequest, res) => {
-  const userData = {
-    name: req.body.name,
-    about: req.body.about,
-  };
-  const notEmptyData = Object.fromEntries(Object.entries(userData).filter(([, value]) => value));
-
-  return User.findByIdAndUpdate(req.user?._id, notEmptyData)
-    .then((user) => res.send({ data: user }))
-    .catch(() => res.status(400).send({ message: 'Пользователь не существует' }));
-});
-
-router.patch('/me/avatar', (req: CustomRequest, res) => User.findByIdAndUpdate(req.user?._id, { avatar: req.body.avatar }, { new: true })
-  .then((user) => res.send({ data: user }))
-  .catch(() => res.status(400).send({ message: 'Пользователь не существует' })));
+router.post('/', createUser);
+router.get('/', findUsers);
+router.get('/:userId', findUserById);
+router.patch('/me', updateUserData);
+router.patch('/me/avatar', updateUserAvatar);
 
 export default router;
