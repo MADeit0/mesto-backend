a4e70a33ff59dae1d48e79a7661df97cd9b5cc92
Олег Цыпуштанов <madeit@MacBook-Pro-Oleg.local>
2023-11-26 23:00:12 +0300

    add a message with error codes for cards controllers

--- src/controllers/cards.ts
+++ src/controllers/cards.ts
@@ -4,7 +4,7 @@ import { CustomRequest } from '../types/customRequestType';
 
 export const findCards = (req: Request, res: Response) => Card.find({})
   .then((card) => res.send({ data: card }))
-  .catch(() => res.status(400).send({ message: 'Произошла ошибка' }));
+  .catch(() => res.status(500).send({ message: 'Внутренняя ошибка сервера' }));
 
 export const createCard = (req: CustomRequest, res: Response) => {
   const id = req.user?._id;
@@ -15,12 +15,17 @@ export const createCard = (req: CustomRequest, res: Response) => {
     likes: [],
   })
     .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(400).send(err));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Данные введены некорректно' });
+      }
+      return res.status(500).send({ message: 'Внутренняя ошибка сервера' });
+    });
 };
 
 export const deleteCard = (req: Request, res: Response) => Card.findByIdAndDelete(req.params.cardId)
   .then(() => res.send({ message: 'Карточка удалена успешно' }))
-  .catch(() => res.status(400).send({ message: 'Произошла ошибка' }));
+  .catch(() => res.status(404).send({ message: 'Карточка с указанным _id не найдена' }));
 
 export const likeCard = (req: CustomRequest, res: Response) => Card.findByIdAndUpdate(
   req.params.cardId,
@@ -28,7 +33,18 @@ export const likeCard = (req: CustomRequest, res: Response) => Card.findByIdAndU
   { new: true },
 )
   .then((card) => res.send({ data: card }))
-  .catch(() => res.status(400).send({ message: 'Произошла ошибка' }));
+  .catch((err) => {
+    switch (err.name) {
+      case 'ValidationError':
+        res.status(400).send({ message: 'Переданы некорректные данные для постановки лайка' });
+        break;
+      case 'CastError':
+        res.status(404).send({ message: 'Передан несуществующий _id карточки' });
+        break;
+      default:
+        res.status(500).send({ message: 'Внутренняя ошибка сервера' });
+    }
+  });
 
 export const dislikeCard = (req: CustomRequest, res: Response) => Card.findByIdAndUpdate(
   req.params.cardId,
@@ -36,4 +52,15 @@ export const dislikeCard = (req: CustomRequest, res: Response) => Card.findByIdA
   { new: true },
 )
   .then((card) => res.send({ data: card }))
-  .catch(() => res.status(400).send({ message: 'Произошла ошибка' }));
+  .catch((err) => {
+    switch (err.name) {
+      case 'ValidationError':
+        res.status(400).send({ message: 'Переданы некорректные данные для снятии лайка' });
+        break;
+      case 'CastError':
+        res.status(404).send({ message: 'Передан несуществующий _id карточки' });
+        break;
+      default:
+        res.status(500).send({ message: 'Внутренняя ошибка сервера' });
+    }
+  });
