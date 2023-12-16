5aed84037e4647bfa109c0aadab5171a1b48d73e
Олег Цыпуштанов <madeit@MacBook-Pro-Oleg.local>
2023-12-16 16:48:50 +0300

    fix: change the findOneAndDelete() method to findByIdAndDelete() in the deleteCard function

--- src/controllers/cards.ts
+++ src/controllers/cards.ts
@@ -30,16 +30,19 @@ export const deleteCard = (
   req: CardRequest,
   res: Response,
   next: NextFunction,
-) => Card.findOneAndDelete({
-  _id: req.params.cardId,
-  owner: req.user?._id,
-})
-  .orFail(new NotFoundError('карточка не найдена'))
+) => Card.findById(req.params.cardId)
+  .orFail(new NotFoundError('Передан несуществующий _id карточки'))
+  .then((card) => {
+    if (card.owner.toString() !== req.user?._id) {
+      throw new ForbiddenRequestError('Нет прав на удаление этой карточки');
+    }
+    return Card.findByIdAndDelete(req.params.cardId);
+  })
   .then(() => res.send({ message: 'Карточка удалена успешно' }))
   .catch((err) => {
     switch (true) {
       case err.name === 'CastError':
-        next(new BadRequestError('Передан невалидный _id карточки'));
+        next(new BadRequestError('Передан невалидный _id'));
         break;
       default:
         next(err);
